41196204af79c0e6b08b2bd0210493715d9b6e3d
samet312 <samet312@gmail.com>
2021-05-18 10:32:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,29 @@
+# instagram-clone-vue
+
+## TODOS
+
+- [x] Vue ve React arasındaki fark. vue-cli ile proje kurulumu
+- [x] CSS alt yapısı ve tipografi. projedeki (svg) ikonların hazırlanması
+- [x] Sayfaları oluşturalım. Master-Page oluşturalım
+- [x] Header tasarımı ve Anasayfa için layout oluşturalım
+- [x] Proje host etmek için vercel.com deploy konfigrasyonu
+- [ ] Anasayfa'daki post componenti
+- [ ] Anasayfa için api ve infinite-scroll
+- [ ] Anasayfa'daki stories alanı
+- [ ] Yorum gönder için api ve implemente edilmesi
+- [ ] Options modal componenti ve animasyon kullanımı
+- [ ] Keşfet sayfası için api ve layout tasarımı
+- [ ] Post modal componenti ve keşfet sayfasına implemente edilmesi
+- [ ] Profil sayfası ve sekmeler için route ayarı
+
+## Project setup, development and build
+
+```
+yarn install
+
+# development
+yarn serve
+
+# production
+yarn build
+```
---
+++ src/views/home/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <div class="timeline">
+      <h1>Home</h1>
+
+      <br />
+      <br />
+
+      <Post />
+
+      <br />
+      <br />
+      <br />
+    </div>
+
+    <div class="sidebar">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eius
+        vero. Alias aut blanditiis commodi consectetur eligendi, et minima
+        officiis omnis optio pariatur quod quos repudiandae rerum sed tempora
+        voluptatibus?
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from '@/components/Post'
+export default {
+  name: 'Home',
+  components: { Post }
+}
+</script>
+
+<style scoped>
+.home {
+  max-width: 605px;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media (--t) {
+    max-width: none;
+    display: grid;
+    grid-template-columns: 1fr 295px;
+    grid-gap: 30px;
+  }
+}
+
+.sidebar {
+  display: none;
+  @media (--t) {
+    display: block;
+  }
+}
+</style>
